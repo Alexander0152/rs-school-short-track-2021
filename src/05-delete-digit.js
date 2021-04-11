40369d4159a76437a7616c7817bb4c4d43946f04
Alexander0152 <66972872+Alexander0152@users.noreply.github.com>
2021-04-11 19:32:22 +0300

    add: 5,6,7

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,16 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const arr = n.toString().split('');
+  const numbers = [];
+  for (let i = 0; i < arr.length; i++) {
+    const number = arr.slice();
+    number.splice(i, 1);
+    numbers[i] = number.join('');
+  }
+  /* eslint-disable prefer-spread */
+  return Math.max.apply(Math, numbers);
 }
 
 module.exports = deleteDigit;
--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,48 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function replace(str, index, replacement) {
+  return str.substr(0, index) + replacement + str.substr(index + replacement.length);
+}
+
+function getVersion(newArray, el, position) {
+  for (let i = 0; i < position; i++) {
+    if (newArray[i] === el) {
+      const newNumber = (parseInt(el.charAt(el.length - 2), 10) + 1);
+      /* eslint-disable no-param-reassign */
+      el = replace(el, el.length - 2, newNumber.toString());
+    }
+  }
+  return el;
+}
+
+function renameFiles(names) {
+  if (names.length === 0) {
+    return names;
+  }
+
+  const newArray = [];
+  newArray.push(names[0]);
+  let pushed = false;
+
+  for (let j = 1; j < names.length; j++) {
+    let el = names[j];
+    pushed = false;
+    for (let i = 0; i < j; i++) {
+      if (newArray[i] === el) {
+        el += '(1)';
+        el = getVersion(newArray, el, j);
+        newArray.push(el);
+        pushed = true;
+        break;
+      }
+    }
+    if (!pushed) {
+      newArray.push(el);
+    }
+  }
+
+  return newArray;
 }
 
 module.exports = renameFiles;
