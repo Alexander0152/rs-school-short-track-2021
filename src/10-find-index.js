b725529d5293ad80eb0917d89ca8a58130e73a51
Alexander0152 <66972872+Alexander0152@users.noreply.github.com>
2021-04-12 11:20:33 +0300

    add: 10, 11 tasks

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,25 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let firstPos = 0;
+  let lastPos = array.length - 1;
+  let result = -99;
+  let isFound = false;
+  let center;
+
+  while (isFound === false && firstPos <= lastPos) {
+    center = Math.floor((firstPos + lastPos) / 2);
+    if (array[center] === value) {
+      isFound = true;
+      result = center;
+    } else if (array[center] > value) {
+      lastPos = center - 1;
+    } else {
+      firstPos = center + 1;
+    }
+  }
+  return result;
 }
 
 module.exports = findIndex;
--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,30 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const result = [];
+  const withoutDuplication = [];
+  let arr = str.split('');
+  withoutDuplication.push(arr[0]);
+  for (let i = 1; i < str.length; i++) {
+    if (arr[i] !== arr[i - 1]) {
+      withoutDuplication.push(arr[i]);
+    }
+  }
+  let count;
+  withoutDuplication.forEach((el) => {
+    count = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (el === arr[i]) {
+        count++;
+      } else break;
+    }
+    arr = arr.slice(count, arr.length);
+    if (count > 1) {
+      result.push(count.toString() + el);
+    } else result.push(el);
+  });
+  return result.join('');
 }
 
 module.exports = encodeLine;
